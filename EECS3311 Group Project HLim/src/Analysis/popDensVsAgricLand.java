cc82d40c0668d2449ec814b04dc3cbac0d6235ed
Emad Chaudhry <emadtc@gmail.com>
2022-11-28 17:37:15 -0500

    all analysis classes complete

--- EECS3311 Group Project HLim/src/Analysis/popDensVsAgricLand.java
+++ EECS3311 Group Project HLim/src/Analysis/popDensVsAgricLand.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.Vector;
 
 import dataGetters.*;
-public class popDensVsAgricLand implements MethodStrategy{
-	
+public class popDensVsAgricLand {
+	String x = "Population density";
+	String y = "% Agricultural Land";
 	public static Vector<Double> performAnalysis(String y1, String y2, String CC){
 		int analysis1 = 6;
 		int analysis2 = 7;
@@ -20,7 +21,7 @@ public class popDensVsAgricLand implements MethodStrategy{
 		data1.setY2(y2);
 		data1.setFinalUrl();
 		data1.dataRetrievedList = new Vector<Double>();
-		list1 = data1.getData(data1.finalUrl);
+		list1 = data1.getData(data1.getFinalURL());
 		
 		getDataAgriculture data2 = (getDataAgriculture) dataFactory.createdataGetter(analysis2);
 		data2.setCC(CC);
@@ -28,7 +29,7 @@ public class popDensVsAgricLand implements MethodStrategy{
 		data2.setY2(y2);
 		data2.setFinalUrl();
 		data2.dataRetrievedList = new Vector<Double>();
-		list2 = data2.getData(data2.finalUrl);
+		list2 = data2.getData(data2.getFinalURL());
 
 		
 		for (int i = 0; i < list1.size(); i++) {
@@ -48,11 +49,6 @@ public class popDensVsAgricLand implements MethodStrategy{
 		for(int i = 0; i < output.size(); i++) {
 			System.out.println(output.get(i));
 		}
-	}
-	@Override
-	public void methodAnalysis(String y1, String y2, String CC) {
-		popDensVsAgricLand.performAnalysis(y1, y2, CC);
-		
 	}
 
 }
